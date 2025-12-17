b1d2a25c60621afc173827c15a3ae5ac6ce2fa73
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-17 23:18:12 +0000

    remove unneeded code after bcrypt downgrade

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from fastapi.security import OAuth2PasswordBearer
 
 from . import database, models
 from .routers import flights, users
+
+# This tells FastAPI where to look for the token
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # creates all the tables in the database (Only run if you haven't run the SQL scripts)
 # database.Base.metadata.create_all(bind=database.engine)
@@ -11,7 +15,7 @@ app = FastAPI(title="Airline Booking System API", version="1.0")
 # All flight related endpoints
 app.include_router(flights.router, prefix="/api/v1/flights", tags=["Flights"])
 app.include_router(
-    users.router, prefix="/api/v1/users", tags=["Users adn Authentication"]
+    users.router, prefix="/api/v1/users", tags=["Users and Authentication"]
 )
 
 
