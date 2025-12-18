927fb3552584747b78a51bce21ac9586a0df6c75
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-18 19:30:23 +0000

    create dependencies.py file for centralized dependency injection

---
+++ app/dependencies.py
@@ -0,0 +1,41 @@
+from database import get_db
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+# Import the SQLAlchemy Models, Pydantic Schemas, and DB utilities
+from . import models
+
+# this tells fastapi where to look for the token
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+# These should ideally go in your .env later!
+SECRET_KEY = "a-very-secret-string-12345"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        decoded_jwt = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_email = decoded_jwt.get("sub")
+        if user_email is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+
+    db_user = db.query(models.User).filter(models.User.Email == user_email).first()
+    if db_user is None:
+        raise credentials_exception
+
+    return db_user
