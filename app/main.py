9705d0c6a517e66e349ed43d97c851d1cf05aec3
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-19 15:30:18 +0000

    include bookings route in main app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 
-from . import database
-from .routers import flights, users
+from .routers import bookings, flights, users
 
 # creates all the tables in the database (Only run if you haven't run the SQL scripts)
 # database.Base.metadata.create_all(bind=database.engine)
@@ -13,6 +12,7 @@ app.include_router(flights.router, prefix="/api/v1/flights", tags=["Flights"])
 app.include_router(
     users.router, prefix="/api/v1/users", tags=["Users and Authentication"]
 )
+app.include_router(bookings.router, prefix="/api/v1/bookings", tags=["Bookings"])
 
 
 @app.get("/api/v1/health")
