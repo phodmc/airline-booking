ce2bb2a8ed122460e8a3bbb216a4ca48926577bd
Pho <115372840+phodmc@users.noreply.github.com>
2025-12-21 21:21:20 +0000

    feat: add flight creation route that requires an admin privileged user

--- app/routers/flights.py
+++ app/routers/flights.py
@@ -5,6 +5,9 @@ from typing import List, Optional
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy import Date, cast
 from sqlalchemy.orm import Session, aliased, joinedload
+from starlette.types import HTTPExceptionHandler
+
+from app.dependencies import get_admin_user
 
 # Import the SQLAlchemy Models and Pydantic Schemas
 from .. import models, schemas
@@ -70,3 +73,48 @@ def get_flight_details(flight_id: int, db: Session = Depends(get_db)):
     # The FlightRead schema doesn't yet include the InventoryRead list, but we can
     # create a detailed schema later that does. For now, let's just return the core FlightRead data.
     return flight
+
+
+# Create flights by user with admin privileges
+@router.post("", response_model=schemas.FlightRead)
+def create_flight(
+    flight_in: schemas.FlightCreate,
+    db: Session = Depends(get_db),
+    admin_user: models.User = Depends(get_admin_user),
+):
+    try:
+        new_flight = models.Flight(
+            FlightNumber=flight_in.FlightNumber,
+            DepartureAirportID=flight_in.DepartureAirportID,
+            ArrivalAirportID=flight_in.ArrivalAirportID,
+            AircraftID=flight_in.AircraftID,
+            DepartureDateTime=flight_in.DepartureDateTime,
+            ArrivalDateTime=flight_in.ArrivalDateTime,
+            BasePrice=flight_in.BasePrice,
+            Status=flight_in.Status,
+        )
+
+        db.add(new_flight)
+        db.flush()  # send record to db, generates id but data not commited
+
+        for item in flight_in.inventory_items:
+            inventory = models.FlightInventory(
+                FlightID=new_flight.FlightID,
+                ClassCode=item.ClassCode,
+                BaseFare=item.BaseFare,
+                TotalSeats=item.TotalSeats,
+            )
+
+            db.add(inventory)
+
+        # save all db transactions permanently
+        db.commit()
+
+        db.refresh(new_flight)
+        return new_flight
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(
+            status_code=500, detail=f"Failed to create flight: {str(e)}"
+        )
